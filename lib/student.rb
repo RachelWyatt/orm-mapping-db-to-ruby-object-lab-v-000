b5d343422c5997cb8fe4648d3a95657e7f3a18d2
Rachel Wyatt <rw126@evansville.edu>
2018-11-16 16:10:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,10 +38,10 @@ class Student
       WHERE grade = "9"
     SQL
  
-    all_in_9 = DB[:conn].execute(sql).map do |row|
+  DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
-    all_in_9
+
   end
   
   def save
